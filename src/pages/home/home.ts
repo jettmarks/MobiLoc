016c9de9276beb73fab60e814c72fa315255230b
jett <jettmarks@gmail.com>
2017-05-29 19:52:51 -0400

    LE-1 Add Leaflet Map to Side Menu template
    - Added leaflet import.
    - Added div for the map along with a CSS class for height.
    - Linked in the stylesheet.
    - Setup the Open Street Map tiles.
    - Pointed the starting location to Atlanta.
    
    Further notes on the ticket.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import 'leaflet';
 
 @Component({
   selector: 'page-home',
@@ -8,6 +9,16 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
 
   constructor(public navCtrl: NavController) {
+
+  }
+
+  ngOnInit(): void {
+    var map = L.map('map')
+      .setView([33.75, -84.40], 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+    }).addTo(map);
 
   }
 
