eb91a4a780ebc1313a2ccc97040bb17ae1c5e512
jett <jettmarks@gmail.com>
2017-07-02 22:16:03 -0400

    LE-21 Add authentication options
    Good bit of notes behind the procedure and on the ticket.
    
    A picture would help too.
    
    Right now,
    - loading the page triggers a login against Google and nothing else.
    - No logout defined.
    - This can happen silently if the user is already logged in; possible to just let them know they have been identified.
    - Haven't started playing with the data that comes back.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component, ViewChild } from '@angular/core';
-import { Nav, Platform } from 'ionic-angular';
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
+import {Component, ViewChild} from "@angular/core";
+import {Nav, Platform} from "ionic-angular";
+import {StatusBar} from "@ionic-native/status-bar";
+import {SplashScreen} from "@ionic-native/splash-screen";
 
-import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
+import {HomePage} from "../pages/home/home";
+import {ListPage} from "../pages/list/list";
+import {LoginPage} from "../pages/login/login";
 
 @Component({
   templateUrl: 'app.html'
@@ -22,7 +23,8 @@ export class MyApp {
     // used for an example of ngFor and navigation
     this.pages = [
       { title: 'Home', component: HomePage },
-      { title: 'List', component: ListPage }
+      { title: 'List', component: ListPage },
+      { title: 'Login', component: LoginPage },
     ];
 
   }
@@ -32,7 +34,7 @@ export class MyApp {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       this.statusBar.styleDefault();
-      this.splashScreen.hide();
+      // this.splashScreen.hide();
     });
   }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,39 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import {BrowserModule} from "@angular/platform-browser";
+import {ErrorHandler, NgModule} from "@angular/core";
+import {IonicApp, IonicErrorHandler, IonicModule} from "ionic-angular";
+import {CloudModule, CloudSettings} from "@ionic/cloud-angular";
 
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
+import {MyApp} from "./app.component";
+import {HomePage} from "../pages/home/home";
+import {ListPage} from "../pages/list/list";
 
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { GeoLocComponent } from '../components/geo-loc/geo-loc';
-import { MapComponent } from '../components/map/map';
-import { MarkersComponent } from '../components/markers/markers';
+import {StatusBar} from "@ionic-native/status-bar";
+import {SplashScreen} from "@ionic-native/splash-screen";
+
+import {GeoLocComponent} from "../components/geo-loc/geo-loc";
+import {MapComponent} from "../components/map/map";
+import {MarkersComponent} from "../components/markers/markers";
+import {LoginPage} from "../pages/login/login";
+
+const cloudSettings: CloudSettings = {
+  'core': {
+    'app_id': 'd7271bbc'
+  },
+  'auth': {
+    'google': {
+      'webClientId': '989278627857-1bei5m1ek171qcb0ork8s1brnuacm84p.apps.googleusercontent.com',
+      // 'offline': true,
+      // 'scope': "email profile"
+    }
+  }
+};
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
     ListPage,
+    LoginPage,
     GeoLocComponent,
     MapComponent,
     MarkersComponent
@@ -24,12 +41,14 @@ import { MarkersComponent } from '../components/markers/markers';
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
+    CloudModule.forRoot(cloudSettings),
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    LoginPage,
   ],
   providers: [
     GeoLocComponent,
