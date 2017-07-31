d823a10e3e8633e32c31e1f4a7229267e5681224
jett <jettmarks@gmail.com>
2017-07-30 21:26:58 -0400

    LE-32: Returning Token with each request
    * Adds Creds service to cache the token obtained during login.
    * Login Service (BadgeService at this time) has custom interceptor
    to pull the Authentication-Token header's value.
    * Overall resource config will add a Authorization header on each
    request.

--- src/providers/resources/badge/badge.service.provider.ts
+++ src/providers/resources/badge/badge.service.provider.ts
@@ -1,9 +1,28 @@
-import {Restangular} from "ngx-restangular/dist/esm/src";
+import {Creds} from "../../creds/creds.service";
 import {OpaqueToken} from "@angular/core";
+import {Restangular} from "ngx-restangular/dist/esm/src";
 
 export const BADGES_REST = new OpaqueToken('BadgeResource');
 
-export function BadgesRestFactory(restangular: Restangular) {
+export function BadgesRestFactory(
+  restangular: Restangular,
+) {
+  /* Configure this particular resource to capture the Authentication-Token header. */
+  restangular.withConfig(
+    (configurer) => {
+      configurer.addResponseInterceptor(
+        (data, operation, what, url, response) => {
+          if (response.headers.get('Authentication-Token')) {
+            Creds.setAuthToken(response.headers.get('Authentication-Token'));
+          }
+          /* Response Interceptors are required to return the data property */
+          return data;
+        }
+      );
+    }
+  );
+
+  /* The path for this API endpoint */
   return restangular.service('login');
 }
 
